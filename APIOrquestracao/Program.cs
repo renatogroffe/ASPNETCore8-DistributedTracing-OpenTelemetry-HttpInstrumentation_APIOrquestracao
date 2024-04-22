73dbbc419bbf37984df28ffb751218578c856928
Renato Groffe <renato.groffe@yahoo.com.br>
2024-04-22 07:45:45 -0300

    Ajustes

--- APIOrquestracao/Program.cs
+++ APIOrquestracao/Program.cs
@@ -1,9 +1,18 @@
+using APIOrquestracao.Clients;
 using APIOrquestracao.Tracing;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
-using APIOrquestracao.Clients;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Documentacao do OpenTelemetry:
+// https://opentelemetry.io/docs/instrumentation/net/getting-started/
+
+// Integracao do OpenTelemetry com Jaeger em .NET:
+// https://github.com/open-telemetry/opentelemetry-dotnet/tree/e330e57b04fa3e51fe5d63b52bfff891fb5b7961/docs/trace/getting-started-jaeger#collect-and-visualize-traces-using-jaeger
+
+// Documentacaoo do Jaeger:
+// https://www.jaegertracing.io/docs/1.56/
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
